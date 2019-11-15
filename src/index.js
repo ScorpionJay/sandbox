3cd4d0305f5c089506804dd8b329db0a436cf672
Jay <jay.wang.scorpion@gmail.com>
2019-11-15 13:20:51 +0800

    init
    react init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
